051b3cf6eea3cd63f842100f599c9570d5f40ec5
chavanskc <140434094+chavanskc@users.noreply.github.com>
2023-07-24 13:33:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 # First Main File
 import streamlit
+import pandas
 streamlit.title('......Dinner Time')
 streamlit.header('Menu')
 streamlit.text('🥣  Dosey')
 streamlit.text('🥗 Uppittu')
 streamlit.text('🥑🍞 Avocado Toast')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
                 
                  
