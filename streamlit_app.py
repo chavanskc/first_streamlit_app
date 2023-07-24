d028197cdbfa1ad92a7ab8b89fc16f99653d298d
chavanskc <140434094+chavanskc@users.noreply.github.com>
2023-07-24 14:20:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,12 @@ streamlit.header('Menu')
 streamlit.text('🥣  Dosey')
 streamlit.text('🥗 Uppittu')
 streamlit.text('🥑🍞 Avocado Toast')
+
+# Load the Fruit List
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+stremlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
 
                 
                  
