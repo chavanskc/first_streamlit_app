948904ba0301a6803680faf73a8f433c04cc14c9
chavanskc <140434094+chavanskc@users.noreply.github.com>
2023-07-25 19:22:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,11 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? - convert JSON into table/frame
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do? Show as table (Data Frame)
+streamlit.dataframe(fruityvice_normalized)
 
 
                 
